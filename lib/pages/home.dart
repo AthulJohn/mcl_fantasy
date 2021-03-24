8ff4b043050025bf7b1d6c8bd0e15059805a3a14
Athul John <athuljohn@ieee.org>
2021-03-24 18:45:31 +0530

    added pages

---
+++ lib/pages/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.network(
+                "https://drive.google.com/uc?export=view&id=1pkRm-_6c_3JOTD0vWOXfSB_EI8ImccJY"),
+            Card(
+              child: Column(
+                children: [
+                  Text('Signin'),
+                  TextField(),
+                  TextField(),
+                ],
+              ),
+            ),
+            Image.network(
+                "https://drive.google.com/uc?export=view&id=1WSDax83g2k3PC_R3-XU_YriW1F2Ulos3"),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/signup.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:mcl_fantasy/auth/sign.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.network(
+                "https://drive.google.com/uc?export=view&id=1pkRm-_6c_3JOTD0vWOXfSB_EI8ImccJY"),
+            Card(
+              child: Column(
+                children: [
+                  TextButton(
+                    child: Text('Signin'),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.resolveWith<Color>((_) {
+                        return Colors.red;
+                      }),
+                      shape: MaterialStateProperty.resolveWith<OutlinedBorder>(
+                          (_) {
+                        return RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15));
+                      }),
+                    ),
+                    onPressed: () {
+                      AuthService().signInWithGoogle();
+                    },
+                  ),
+                  Text('Please sign in using your MACE mail id.'),
+                ],
+              ),
+            ),
+            Image.network(
+                "https://drive.google.com/uc?export=view&id=1WSDax83g2k3PC_R3-XU_YriW1F2Ulos3"),
+          ],
+        ),
+      ),
+    );
+  }
+}
